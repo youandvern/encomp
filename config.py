ee54c426b189a466725db0c056a6423cddc2713b
Ubuntu <ubuntu@ip-10-0-0-204.ec2.internal>
2020-12-28 00:33:15 +0000

    Changes made from server to get site working. Updated environment and requirements.

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xe8\x0f\x054-"\xa3\x8a\xa5\xea\x1b\xbc\xc9%<\xcf'
 
-    MONGODB_SETTINGS = [{'ALIAS': 'default', 'DB' : 'ENCOMP_Main'}, {'ALIAS' : 'calc_db', 'DB':'ENCOMP_calcdata'}]
-    # MONGODB_SETTINGS = [{'ALIAS': 'default', 'DB' : 'ENCOMP_Main', 'host':'52.201.7.126:27017' }, {'ALIAS' : 'calc_db', 'DB':'ENCOMP_calcdata', 'host':'52.201.7.126:27017' }]
+    # MONGODB_SETTINGS = [{'ALIAS': 'default', 'DB' : 'ENCOMP_Main'}, {'ALIAS' : 'calc_db', 'DB':'ENCOMP_calcdata'}]
+    MONGODB_SETTINGS = [{'ALIAS': 'default', 'DB' : 'ENCOMP_Main', 'host':'52.201.7.126:27017' }, {'ALIAS' : 'calc_db', 'DB':'ENCOMP_calcdata', 'host':'52.201.7.126:27017' }]
