108f68e338130477d26dc3d0a1c3b9b912ec6ba9
AndrewYoung <ayounguw@gmail.com>
2020-12-28 22:33:09 -0800

    Late night cosmetic updates to forms including spacing, sizing, and layouts. Updated print calc report javascript to work without refreshing page.

--- application/static/js/calcsheet.js
+++ application/static/js/calcsheet.js
@@ -1,13 +1,23 @@
-window.MathJax = {
-  startup: {
-    ready: () => {
-      MathJax.startup.defaultReady();
-      MathJax.startup.promise.then(() => {
-        var pagedscript = document.createElement('script');
-        pagedscript.setAttribute('src','https://unpkg.com/pagedjs/dist/paged.polyfill.js');
-        document.head.appendChild(pagedscript);
-        console.log('MathJax initial typesetting complete');
-      });
-    }
-  }
-};
+// window.MathJax = {
+//   startup: {
+//     ready: () => {
+//       MathJax.startup.defaultReady();
+//       MathJax.startup.promise.then(() => {
+//         // var pagedscript = document.createElement('script');
+//         // pagedscript.setAttribute('src','https://unpkg.com/pagedjs/dist/paged.polyfill.js');
+//         // document.head.appendChild(pagedscript);
+//         // console.log('MathJax initial typesetting complete');
+//         // window.location.href = window.location.href;
+//       });
+//     }
+//   }
+// };
+
+function postLoadedScript() {
+  var pagedscript = document.createElement('script');
+  pagedscript.setAttribute('src','https://unpkg.com/pagedjs/dist/paged.polyfill.js');
+  document.head.appendChild(pagedscript);
+  // console.log('MathJax initial typesetting complete');
+}
+
+document.addEventListener("load", postLoadedScript());
