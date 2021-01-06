ca7d8e0fb788fc8414c801c4c46981bc984cf599
AndrewYoung <ayounguw@gmail.com>
2021-01-05 21:22:15 -0800

    combined print calcreport into single route. added button and paged styling to calcsheet.css. Completely rewrote calcsheet.js to work with new print button and combined view/print report. Downloaded pagedjs instead of using CDN.

--- application/static/js/calcsheet.js
+++ application/static/js/calcsheet.js
@@ -1,23 +1,48 @@
-// window.MathJax = {
-//   startup: {
-//     ready: () => {
-//       MathJax.startup.defaultReady();
-//       MathJax.startup.promise.then(() => {
-//         // var pagedscript = document.createElement('script');
-//         // pagedscript.setAttribute('src','https://unpkg.com/pagedjs/dist/paged.polyfill.js');
-//         // document.head.appendChild(pagedscript);
-//         // console.log('MathJax initial typesetting complete');
-//         // window.location.href = window.location.href;
-//       });
-//     }
-//   }
-// };
-
-function postLoadedScript() {
-  var pagedscript = document.createElement('script');
-  pagedscript.setAttribute('src','https://unpkg.com/pagedjs/dist/paged.polyfill.js');
-  document.head.appendChild(pagedscript);
-  // console.log('MathJax initial typesetting complete');
+var print_button_section = document.getElementById("print_button_section");
+if (print_button_section) {
+  console.log("found button");
+} else {
+  console.log("button not foung");
 }
 
-document.addEventListener("load", postLoadedScript());
+
+function page_format_and_print() {
+
+  function myprintfunction(module) {
+    setTimeout(() => {
+  		window.PagedPolyfill.preview().then(module => window.print());
+  	}, 1000);
+  }
+
+  let pagedscript = import('/static/js/paged.polyfill.js?v=1').then(module => myprintfunction(module)); // .then(module => myprintfunction(module)); // .then(window.print());
+  // console.log(pagedscript);
+  window.PagedConfig = {
+		auto: false // ,
+		// after: (flow) => { console.log("after", flow) },
+	};
+
+
+}
+
+function hide_print_button() {
+  if (print_button_section) {
+    console.log("button hidden");
+    print_button_section.style.display = "none";
+  } else {
+    console.log("button not found")
+  }
+
+}
+
+function auto_print_report(){
+  hide_print_button();
+
+  document.addEventListener("load",MathJax.Hub.Queue( () => page_format_and_print()));
+}
+
+function click_print_report(){
+  hide_print_button();
+  page_format_and_print();
+}
+
+// document.addEventListener("load", postLoadedScript());
