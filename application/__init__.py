b7ae1dcc0987e550bf991d0b1f561118f3d1940d
AndrewYoung <ayounguw@gmail.com>
2021-02-19 09:22:03 -0800

    Downloaded flask_mail and added contact page.

--- application/__init__.py
+++ application/__init__.py
@@ -2,9 +2,11 @@
 from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
+from flask_mail import Mail
 # import redis
 from flask_kvsession import KVSessionExtension
 from simplekv.memory import DictStore
+import os
 
 store = DictStore()
 
@@ -12,6 +14,14 @@ store = DictStore()
 application = Flask(__name__)
 application.config.from_object(Config)
 KVSessionExtension(store, application) # overrides flask session to use simplekv storage (increase size)
+
+application.config["MAIL_SERVER"] = 'smtp.zoho.com'
+application.config["MAIL_PORT"] = 465
+application.config["MAIL_USE_SSL"] = True
+application.config["MAIL_USERNAME"] = 'team@encompapp.com'
+application.config["MAIL_PASSWORD"] = os.environ['ZOHO_EMAIL_PASSWORD']
+
+mail = Mail(application)
 
 db = MongoEngine()
 db.init_app(application)
