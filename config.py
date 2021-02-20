76b48c78e5d5bc7089e6addab4412efb2a9e4a8b
AndrewYoung <ayounguw@gmail.com>
2021-02-20 09:07:12 -0800

    Added playsinline to all videos so that they play on mobile devices.

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+from private import P_SECRET_KEY, P_MONGODB_SETTINGS
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xe8\x0f\x054-"\xa3\x8a\xa5\xea\x1b\xbc\xc9%<\xcf'
+    SECRET_KEY = P_SECRET_KEY
 
     # MONGODB_SETTINGS = [{'ALIAS': 'default', 'DB' : 'ENCOMP_Main'}, {'ALIAS' : 'calc_db', 'DB':'ENCOMP_calcdata'}]
-    MONGODB_SETTINGS = [{'ALIAS': 'default', 'DB' : 'ENCOMP_Main', 'host':'52.201.7.126:27017' }, {'ALIAS' : 'calc_db', 'DB':'ENCOMP_calcdata', 'host':'52.201.7.126:27017' }]
+    MONGODB_SETTINGS = P_MONGODB_SETTINGS
