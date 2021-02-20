e049dbb35843228fc65b503abbe5c81ed15db594
AndrewYoung <ayounguw@gmail.com>
2021-02-19 19:04:27 -0800

    Major work to a second homepage for testing.Also updated torsion calc for alum stair stringer. Updated contact page.

--- application/static/js/active.js
+++ application/static/js/active.js
@@ -1,5 +1,41 @@
 // WINDOW.FOCUS()
 
+
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
+var slideIndex = 1;
+var timeoutVar;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  clearTimeout(timeoutVar);
+  var i;
+  var slides = document.getElementsByClassName("index-video-slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slide-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " slide-active";
+  timeoutVar = setTimeout(function() { plusSlides(1); }, 6000); // Change image every 6 seconds
+}
+
+
 function projCalcDisplay(){
   //show calcs associated with given projects
 }
