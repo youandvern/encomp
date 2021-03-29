e355ce6f517835172d0a8247ce0d761d353ca3e3
AndrewYoung <ayounguw@gmail.com>
2021-03-28 19:58:19 -0700

    Cleaned up ASCE 7-10

--- application/calcscripts/ASCE_7_10_seismic.py
+++ application/calcscripts/ASCE_7_10_seismic.py
@@ -14,7 +14,6 @@ def create_calculation(updated_input={}):
 
 
 
-
     ###   DEFINE TITLE, DESCRIPTION, ASSUMPTIONS, AND INPUTS   ###
 
     CalculationTitle('ASCE 7-10 Seismic Design')
@@ -29,7 +28,7 @@ def create_calculation(updated_input={}):
     site_class = DeclareVariable('SiteClass', 'D', '', 'Site site class of soil conditions at project', input_type="select", input_options=site_class_options)
     Ss = DeclareVariable('S_s', 1.888, 'g', 'Spectral response acceleration parameter at short periods')
     S1 = DeclareVariable('S_1', 0.621, 'g', 'Spectral response acceleration parameter at 1s period')
-    section_name = DeclareVariable('Section', 'HSS12X3X5/16', '', 'AISC section size')
+    # section_name = DeclareVariable('Section', 'HSS12X3X5/16', '', 'AISC section size')
 
 
 
@@ -70,16 +69,16 @@ def create_calculation(updated_input={}):
     c_v = CalcVariable('C_v', 0.2*s_ds, 'g', result_check=True)
     BodyText('(allowable)')
     c_va = CalcVariable('C_{va}', c_v/1.4, 'g')
-
-    section = AISCSectionsRectangular.objects(AISC_name=section_name.value).first()
-    nomB = CalcVariable('b_{nom}', section.B, 'in', 'Nominal section width', result_check=True)
-    actualB = CalcVariable('b_{des}', section.b, 'in', 'Actual (design) section width', result_check=True)
-
-    testervar = CalcVariable('Long_{sub}', SQRT(Fa + Fv**Fa - POW((0.001*c_s*c_sa/c_va),3))+s_ms*19+R/I_e*Ss*99916, 'g')
-
-    anothervar = CalcVariable('V_{env}', POW((s_m1+s_d1)/s_ms,4)-POW(s_ds, 4),'g')
-
-    compare = CheckVariable(s_m1, '>', s_ms, result_check=True)
+    #
+    # section = AISCSectionsRectangular.objects(AISC_name=section_name.value).first()
+    # nomB = CalcVariable('b_{nom}', section.B, 'in', 'Nominal section width', result_check=True)
+    # actualB = CalcVariable('b_{des}', section.b, 'in', 'Actual (design) section width', result_check=True)
+    #
+    # testervar = CalcVariable('Long_{sub}', SQRT(Fa + Fv**Fa - POW((0.001*c_s*c_sa/c_va),3))+s_ms*19+R/I_e*Ss*99916, 'g')
+    #
+    # anothervar = CalcVariable('V_{env}', POW((s_m1+s_d1)/s_ms,4)-POW(s_ds, 4),'g')
+    #
+    # compare = CheckVariable(s_m1, '>', s_ms, result_check=True)
 
     calculation_sum = {'head':HeadCollection.head_instances, 'assum': AssumCollection.assum_instances, 'setup':SetupCollection.setup_instances, 'calc':CalcCollection.calc_instances, 'foot':FootCollection.foot_instances}
     return calculation_sum
